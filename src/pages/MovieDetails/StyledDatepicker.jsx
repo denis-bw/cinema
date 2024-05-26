afb4cc5acb509f3836b94604a53bd143db0cfa47
Denis Belousov <109822229+denis-bw@users.noreply.github.com>
2024-05-26 19:19:53 +0300

    Bugfix datepiker

--- src/pages/MovieDetails/StyledDatepicker.jsx
+++ src/pages/MovieDetails/StyledDatepicker.jsx
@@ -1,13 +1,14 @@
-import { forwardRef, useState } from "react";
+// src/components/StyledDatepicker.jsx
+import React, { useState } from "react";
 import { format } from "date-fns";
 import DatePicker from "react-datepicker";
 import { CalendarGlobalStyles, TitleWrapper } from "./StyledDatepicker.styled";
-import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 const StyledDatepicker = () => {
   const [selectedDate, setSelectedDate] = useState(Date.now());
 
-  const CustomInput = forwardRef(({ value, onClick }, ref) => {
+  const CustomInput = React.forwardRef(({ value, onClick }, ref) => {
     return (
       <TitleWrapper onClick={onClick} ref={ref}>
         {format(selectedDate, "dd-MM-yyyy")}
